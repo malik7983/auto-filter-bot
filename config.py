eaa5378c8d5dbe0b166a536189a0b4e28d3d312c
Kichuprobotzzz <111451450+Kichuprobotzzz@users.noreply.github.com>
2022-08-17 22:37:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # Bot information
 SESSION = 'Media_search'
 USER_SESSION = 'User_Bot'
-API_ID = 12345
-API_HASH = '0123456789abcdef0123456789abcdef'
-BOT_TOKEN = '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11'
+API_ID = "15691417"
+API_HASH = "0123456789abcdef0123456789abcdef"
+BOT_TOKEN = '5722971062:AAFOzgt0UIAc16bZprpFhuh-ZePYAuyEuhY'
 USERBOT_STRING_SESSION = ''
 
 # Bot settings
@@ -11,13 +11,13 @@ CACHE_TIME = 300
 USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [12345789, 'admin123', 98765432]
-CHANNELS = [-10012345678, -100987654321, 'channelusername']
+ADMINS = [1905251964]
+CHANNELS = [-1001798625022]
 AUTH_USERS = []
 AUTH_CHANNEL = None
 
 # MongoDB information
-DATABASE_URI = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb]?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://Helooooooo:Helooooooo@cluster0.jywee.mongodb.net/?retryWrites=true&w=majority"
 DATABASE_NAME = 'Telegram'
 COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
